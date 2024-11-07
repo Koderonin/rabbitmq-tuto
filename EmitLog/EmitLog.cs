3f474ca2180c51b039279160fe19f8c1c489bc7f
Koderonin <134518981+Koderonin@users.noreply.github.com>
2024-11-07 11:22:49 +0100

    Q-Tutorial 3: Publish/Subscribe (Fanout)

---
+++ EmitLog/EmitLog.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+public class EmitLog
+{
+    public static void Main(string[] args)
+    {
+        ConnectionFactory factory = new() 
+        { 
+            HostName = "localhost",
+            Port = 5672,
+            ClientProvidedName = "Sender",
+            UserName = "vicen",
+            Password = "admin"
+        };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare(exchange: "logs",
+                                type: ExchangeType.Fanout);
+
+        string message = GetMessage(args);
+        byte[] body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "logs",
+                            routingKey: string.Empty,
+                            basicProperties: null,
+                            body: body);
+                            
+        Console.WriteLine($" [x] Sent {message}");
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    static string GetMessage(string[] args)
+    {
+        return (args.Length > 0) ? string.Join(",", args) : "Hello World!";
+    }
+}
---
+++ ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class ReceiveLogs
+{
+    public static void Main(string[] args)
+    {
+        ConnectionFactory factory = new() 
+        { 
+            HostName = "localhost",
+            Port = 5672,
+            ClientProvidedName = "Worker",
+            UserName = "vicen",
+            Password = "admin"
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare(exchange: "logs",
+                                type: ExchangeType.Fanout);
+        // this creates a non-durable, exclusive, autodelete queue with a generated name
+        var queueName = channel.QueueDeclare().QueueName;
+
+        channel.QueueBind(queue: queueName,
+                        exchange: "logs",
+                        routingKey: string.Empty);
+
+        Console.WriteLine(" [*] Waiting for logs.");
+
+        EventingBasicConsumer consumer = new(channel);
+        consumer.Received += (model, ea) =>
+        {
+            byte[] body = ea.Body.ToArray();
+            string message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received {message}");
+            Console.WriteLine(" [x] Done");
+            
+            // here channel could also be accessed as ((EventingBasicConsumer)sender).Model
+            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+        };
+        
+        channel.BasicConsume(queue: queueName,
+                            autoAck: false,
+                            consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
